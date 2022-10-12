57f1d649280bfd3d3155b8d2bf7c84fa2cd6fae4
Sami-Shafi <freelancersami123@gmail.com>
2022-10-12 23:23:14 +0600

    Initial commit
    Generated by create-expo-app 1.1.2.

--- App.js
+++ App.js
@@ -1,51 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	const [name, setName] = useState("Sami");
-	const [newName, setNewName] = useState("");
-
-	// const handleInput = (e) => setNewName(e.target.value);
-
-	return (
-		<View style={styles.container}>
-			<Text>My Name is {name}.</Text>
-			<TextInput
-				style={[styles.mTop20, styles.input]}
-				placeholder="Type New Name.."
-				keyboardType="alphanumeric"
-				value={newName}
-				onChangeText={setNewName}
-			/>
-			<View style={styles.mTop20}>
-				<Button
-					disabled={!newName}
-					title="Click to Change Name"
-					onPress={() => {
-						setName(newName);
-						setNewName("");
-					}}
-				/>
-			</View>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	mTop20: {
-		marginTop: 30,
-	},
-	input: {
-		borderWidth: 1,
-		padding: 10,
-		borderRadius: 5,
-		width: "70%",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
